ea95973e502028c90f4b31f2cec263c436d139cc
David Gómez Quilón <david.gomez.quilon@gmail.com>
2022-06-01 15:30:28 +0200

    [UPDATE] submodules

---
+++ addons/extra-addons/auth_cloud/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": 'Auth Cloud',
+    "summary": 'Authentication from cloud Platform',
+    "version": '15.0.1.0',
+    "depends": ['base'],
+    "category": 'System',
+    "license": "Ideas Positivas Ilex Edition License v1.0",
+    "description":
+        """
+        """,
+    "data": [],
+    "auto_install": True,
+    "installable": True,
+}
