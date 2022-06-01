0a8d81b6b74a2c2c62af125264f117467d58edac
David Gómez Quilón <david.gomez.quilon@gmail.com>
2022-06-01 16:35:24 +0200

    [FIX] licenses

--- addons/extra-addons/auth_cloud/__manifest__.py
+++ addons/extra-addons/auth_cloud/__manifest__.py
@@ -5,7 +5,7 @@
     "version": '15.0.1.0',
     "depends": ['base'],
     "category": 'System',
-    "license": "Ideas Positivas Ilex Edition License v1.0",
+    "license": "OPL-1",
     "description":
         """
         """,
